a1cd7df5697cf7711f43cc20362f0a0bb3508a40
Steel Hart <shionhart@gmail.com>
2017-04-23 16:48:17 -0500

    added the documentation for the transaction class and removed the javadoc generated html pages because they can be generated easy, so there is no point to upload that to vcs

--- Transaction.java
+++ Transaction.java
@@ -1,5 +1,12 @@
 import java.util.*;
 import java.io.*;
+
+/**
+ * Holds the information for client transactions
+ * @author      Shion Steel Hart <shionhart@gmail.com>
+ * @version     1.0
+ * @since       1.0
+ */
 public class Transaction implements Serializable {
 
    private static final long serialVersionUID = 1L;
@@ -7,26 +14,50 @@ public class Transaction implements Serializable {
    private String description;
    private Calendar date;
 
+   /**
+    * Constructor for the Transaction
+    * @param       type of the transaction that is occuring, which can currently be any string
+    * @param       description of the transactions that is occuring
+    */
    public Transaction (String type, String description) {
       this.type = type;
       this.description = description;
       date = new GregorianCalendar();
       date.setTimeInMillis(System.currentTimeMillis());
    }
+
+   /**
+    * Check to see if the date of the transaction matches the Calendar parameter given
+    * @param       date a Calendar date object to be checked against the transactions date
+    * @return      true if the transaction's date matches the Calendar parameter given
+    * @see         Calendar
+    */
    public boolean onDate(Calendar date) {
       return ((date.get(Calendar.YEAR)  == this.date.get(Calendar.YEAR)) &&
               (date.get(Calendar.MONTH) == this.date.get(Calendar.MONTH)) &&
               (date.get(Calendar.DATE)  == this.date.get(Calendar.DATE)));
    }
   
+   /**
+    * Query the transaction's type
+    * @return      Returns the transaction's type
+    */
    public String getType() {
       return type;
    }
    
+   /**
+    * Query the transaction's description
+    * @return      Returns the transaction's description
+    */
    public String getDescription() {
       return description;
    }
   
+   /**
+    * Query the transaction's date
+    * @return      Returns the transaction's date in string format
+    */
    public String getDate() {
       return date.get(Calendar.MONTH) + "/" + date.get(Calendar.DATE) + "/" + date.get(Calendar.YEAR);
    }
